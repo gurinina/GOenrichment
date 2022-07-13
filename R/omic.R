8f29a7d78ae85cbc3f67933a3b17008601144266
gurinina <g.giaever@ubc.ca>
2022-07-13 01:14:22 -0700

    manual, roxygen

--- R/omic.R
+++ R/omic.R
@@ -2,30 +2,27 @@
 #'
 #' GO biological process (BP) genesets from
 #' Downloaded from Gary Bader's lab in July 2022
+#' @name hGOBP.gmt
+#' @format gmt file: a list of BP genesets where names = GO term
 #'
-#' @format .gmt file: a list of BP genesets where names = GO term
-#'   \describe{
-#'      \length{16746}
-#' }
+#'
 #' @source \url{http://baderlab.org/GeneSets}
-"hGOBP.gmt"
-
+NULL
 #' BP genesets from Saccharomyces cerevisiae
 #'
 #' GO biological process (BP) genesets from
 #' Saccharomyces cerevisiae downloaded in January 2022
-#' @format .gmt file: a list of BP genesets where names = GO term
-#'   \describe{
-#'      \length{3227}
-#' }
+#' @name yGOBP.gmt
+#' @format gmt file: a list of BP genesets where names = GO term
+#'
+#'
 #' @source \url{http://current.geneontology.org/products/pages/downloads.html}
-"yGOBP.gmt"
-
+NULL
 #' BP genesets from Saccharomyces cerevisiae
 #'
 #' GO biological process (BP) genesets from
 #' Saccharomyces cerevisiae downloaded in January 2022
-#'
+#' @name dfGOBP
 #' @format Dataframe listing GOIDs and terms
 #'   \describe{
 #'      \item{GOID}{GO ID}
@@ -34,17 +31,18 @@
 #'      \item{geneSetSize}{GO geneset size}
 #' }
 #' @source \url{http://current.geneontology.org/products/pages/downloads.html}
-"dfGOBP"
-
+NULL
 #' Fitness data from Saccharomyces cerevisiae
 #'
 #' Experimental data from G. Giaever & C. Nislow labs
 #'
-#'
+#' @name sampleFitdata
 #' @format numeric matrix with two columns; 4956 gene names in rows
-#'   \describe{
-#'      \dim{4956 2}
-#' }
+#'         num [1:4956, 1:2] -0.0806 0.038 NA 0.7913 0.0984 ...
+#'       \describe{
+#'      \item{chr [1:4956]}{"AAC1" "AAC3" "AAD14" "AAD3" ...}
+#'      \item{chr [1:2]}{"azithromycin_1.75mM" "azithromycin_2mM"}
+#'}
 #'@source \url{https://www.ggcnlab.com/}
-"sampleFITdata"
+NULL
 
