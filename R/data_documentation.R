c630aea50c275407bb2af72131abaea4a03406f8
gurinina <g.giaever@ubc.ca>
2024-11-04 13:27:26 -0800

    @import

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -1,8 +1,17 @@
+#' Homo sapiens GO Biological Process (BP) Gene Sets
+#'
+#' BP gene sets for Homo sapiens downloaded from Gary Bader's lab in July 2022.
+#' This dataset excludes terms annotated by electronic annotation (IEA).
+#'
 #' @name hGOBP.gmt
-#' @title Homo sapiens GO biological process (BP) genesets
-#' @description BP genesets were downloaded from Gary Bader's
-#' lab in July 2022: human_go_bp_no_go_iea_entrezgene.gmt
-#' (biological process minus those annotated using evidence code
-#' inferred by electronic annotation (IEA))
-#' @format gmt file: a list of BP genesets where names = GO term
+#' @docType data
+#' @title Homo sapiens GO Biological Process (BP) Genesets
+#' @description This data provides GO biological process (BP) gene sets, where
+#' each set corresponds to a BP term with genes annotated to it, minus those
+#' with IEA annotations.
+#' @format A list of BP gene sets where each name represents a GO term.
+#' @usage data(hGOBP.gmt)
 #' @source \url{http://current.geneontology.org/products/pages/downloads.html}
+#' @examples
+#' data(hGOBP.gmt)
+#' head(hGOBP.gmt)
